69b7bb495f97547fdbba45bdcfe5ed0affd95b77
brandonglandt <brandonjglandt@gmail.com>
2019-01-31 08:54:39 -0700

    Creating my own scripts

--- lab2_scripts.sh
+++ lab2_scripts.sh
@@ -5,10 +5,17 @@
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
 
-echo "regular expression"
-read
-grep -c '[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}' regex_practice.txt
-read numOfPhoneNum
-grep -E -c "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" regex_practice.txt
-read numEmails
+echo "Enter Filename: "
+read filename
+echo "Enter regex: "
+read regex
+## $filename, $regex
+grep -E $regex $filename
+
+grep -cE '[0-9]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt
+grep -cE '[A-Za-z0-9]+@[A-Za-z0-9]+[A-Za-z]' regex_practice.txt
+grep -E '[303]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt  > phone_results.txt
+grep -E '[A-Za-z0-9]+@[geocities.com]' regex_practice.txt  > email_results.txt
+echo "Enter regex: "
+read regex > command_results.txt
 
